8dc36e4e27dd2701bc7fd0cb9b02cbc0d68fa8ec
Nana2929 <b06102020@ntu.edu.tw>
2022-08-01 17:02:18 +0800

    refine DepTree algorithms

--- src/utils.py
+++ src/utils.py
@@ -7,6 +7,23 @@ class DepRelation():
     CC = 'cc' # coordinating conjunction 
     NEG = 'neg'
 
+class POS():
+    # https://ckip.iis.sinica.edu.tw/CKIP/paper/Sinica%20Corpus%20user%20manual.pdf
+    Na = 'Na'
+    Nb = 'Nb'
+    Nc = 'Nc'
+    Nd = 'Nd'
+    D = 'D'
+    Da = 'Da'
+    Dfa='Dfa'
+    Dfb='Dfb'
+    Di='Di'
+    Dk='Dk'
+    Nouns = [Na, Nb, Nc, Nd]
+    Adverbs =[D, Da, Dfa, Dfb, Di, Dk]
+       
+BOUNDDIST = 5
+CONJDIST = 2
 transition_list = ['但','但是',
 '只是','不過','美中不足',
 '可惜','遺憾','雖然','其他','只怪', '然而', '卻','反而']
